pzinn/hadamard

commit ef25fcaf7ee2fb6f7162b7c25afef15a62a9dedb

add generation decay

hadamard.py
@@ -704,8 +704,9 @@ def best_from(arrays, scores, gens):
     # select
     if B <= config.training_size:
         return arrays, min_scores, min_gens
-    scores, idx = torch.topk(min_scores, k=config.training_size, largest=False, sorted=False)
+    _, idx = torch.topk(min_scores - config.gen_decay * min_gens, k=config.training_size, largest=False, sorted=False)
     arrays = arrays[idx]
+    scores = scores[idx]
     gens = min_gens[idx]
     return arrays, scores, gens
 

params.py
@@ -29,6 +29,7 @@ n_embd2 = 4*n_embd  # default choice
 n_head = 4
 stacking = 7  # [5,6,7,8,9,10]  # preferably a divisor of nn
 temperature = 1. # [.5, .75, 1, 1.25, 1.5, 1.75, 2]
+gen_decay = .01 #  [0., .02, .04, .06, .08, .1]
 
 # less important parameters
 sample_batch_size = sample_size//10  # for sampling. must be a divisor of sample_size
@@ -79,7 +80,7 @@ except FileNotFoundError:
 
 import ast
 
-hparams_list = ['n', 'n_layer', 'n_embd', 'n_embd2', 'n_head', 'stacking', 'sample_size', 'training_size', 'learning_rate', 'max_iterations', 'training_steps', 'training_batch_size', 'score_function', 'num_improve', 'weight_decay', 'version', 'random_seed', 'sample_batch_size', 'score_batch_size', 'test_set_size', 'num_workers', 'temperature']
+hparams_list = ['n', 'n_layer', 'n_embd', 'n_embd2', 'n_head', 'stacking', 'sample_size', 'training_size', 'learning_rate', 'max_iterations', 'training_steps', 'training_batch_size', 'score_function', 'num_improve', 'weight_decay', 'version', 'random_seed', 'sample_batch_size', 'score_batch_size', 'test_set_size', 'num_workers', 'temperature', 'gen_decay']
 
 # hparams can be updated in command line
 for param in hparams_list:

commit cb4893251932b811292bb49b22685d73c8067f17

fix major bug introduced in ef25fcaf7ee2fb6f7162b7c25afef15a62a9dedb plus minor

hadamard.py
@@ -580,11 +580,9 @@ vec = torch.rand((nn,),device=device,dtype=real_dtype)  # doesn't really matter,
 fft_vec = torch.fft.rfft(vec)
 fft_conj_vec = torch.conj(fft_vec)
 base = torch.arange(nn, device=device)
-def mysort(arrays, scores):
+def derotate(arrays):
     if params.test_score:
         scores1 = score(arrays)
-        if (scores-scores1).abs().max() > eps:
-            raise RuntimeError("score incorrect", scores, scores1, (scores-scores1).abs().max().item(),(scores-scores1).abs().mean().item())
     # 1st phase: permute the 3xnn2 parts
     B = arrays.shape[0]
     m=3
@@ -638,6 +636,12 @@ def apply_aut(idx,arrays0):
     #arrays3[base[:,None,None], torch.arange(3, device=device)[None,:,None],inds3[:,None,:]] = arrays03
     inds3_expanded = inds3.unsqueeze(1).expand(-1, 3, -1)
     arrays3.scatter_(2, inds3_expanded, arrays03)
+    if params.test_score:
+        scores0 = score(arrays0)
+        scores = score(arrays)
+        if (scores0-scores).abs().max() > eps:
+            raise RuntimeError("score incorrect", scores0, scores, (scores0-scores).abs().max().item(),(scores0-scores).abs().mean().item())
+
     return arrays
 
 
@@ -689,9 +693,9 @@ def parallel_improve(arrays, scores, gens):
     # step C: rotate the arrays to a standard form
     start_timer = timer()
     arrays = find_aut(arrays)  # do automorphisms
-    mysort(arrays, scores)  # then the rest
+    derotate(arrays)  # then the rest
     if debugging:
-        print(f"mysort time: {timer() - start_timer}")
+        print(f"derotate time: {timer() - start_timer}")
     return (arrays, scores, gens)
 
 def best_from(arrays, scores, gens):
@@ -708,7 +712,7 @@ def best_from(arrays, scores, gens):
         return arrays, min_scores, min_gens
     _, idx = torch.topk(min_scores * (1 + config.gen_decay * (params.gen - min_gens)), k=config.training_size, largest=False, sorted=False)
     arrays = arrays[idx]
-    scores = scores[idx]
+    scores = min_scores[idx]
     gens = min_gens[idx]
     return arrays, scores, gens
 

params.py
@@ -174,11 +174,11 @@ def rotate(array0):
     array3=array[:,:3*nn2].view(-1,3,nn2)
     array1=array[:,3*nn2:]
     # automorphism
-    i = rnd[1].item()
+    #i = rnd[1].item()
     #array1.copy_(array01[:,aut_inds1[i]])
     #array3.copy_(array03[:,:,aut_inds3[i]])
-    array1.index_copy_(1,aut_inds1[i],array01)  # does the *inverse* of commented out line
-    array3.index_copy_(2,aut_inds3[i],array03)
+    array1.index_copy_(1,aut_inds1[rnd[1]],array01)  # does the *inverse* of commented out line
+    array3.index_copy_(2,aut_inds3[rnd[1]],array03)
     # symmetry: random permute
     array3.copy_(array3[:,perms[rnd[0]]])  # here can't use index_copy_ because source=target
     # symmetry: second rotation/flip