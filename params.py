8ec54c2f6b048e3c10f80c1a94d8da17619e3c87
Paul Zinn-Justin <pzinn@rs-l-u1twxc.novalocal>
2026-01-23 20:26:59 +1100

    update params

--- params.py
+++ params.py
@@ -11,9 +11,9 @@ n = 148  # size of matrix
 # the parameters below are sweepable: use values, or lists for a sweep
 
 # training parameters
-sample_size = 1_000_000
-training_size = sample_size//20  # must be > test_set_size
-learning_rate = 1e-3
+sample_size = 400_000
+training_size = sample_size//10  # must be > test_set_size
+learning_rate = 2e-3
 training_batch_size = 1024  # for training. much smaller, obviously
 weight_decay = 0.01
 max_iterations = 30
@@ -25,7 +25,7 @@ n_layer = 4
 n_embd = 64
 # n_embd2 = 4*n_embd  # default choice
 n_head = 4
-stacking = 7  # [5,6,7,8,9,10]  # preferably a divisor of nn
+stacking = 5  # [5,6,7,8,9,10]  # preferably a divisor of nn
 temperature = 1.  # [.5, .75, 1, 1.25, 1.5, 1.75, 2]
 
 # less important parameters
