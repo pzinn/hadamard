f5969b8974e61b25000b5794f3ef2d58430d527f
Paul Zinn-Justin <pzinn@rs-l-u1twxc.novalocal>
2026-05-06 10:38:06 +1000

    reinstate fixed improvements

--- hadamard.py
+++ hadamard.py
@@ -162,18 +162,17 @@ def parallel_improve(arrays, scores, gens):
             print(f"improve A1 time: {timer() - start_timer}")
             record_stats(arrays, scores, gens, prefix="improve A1")
         #
-        """
         start_timer = timer()
         if fixed_sums:
             improve_tabu_fixed(arrays, scores)
         else:
-            improve_tabu(arrays, scores)
+            pass
+            #improve_tabu(arrays, scores)
         scores = score(arrays)  # don't trust improve
         if verbose:
             print(f"improve A2 time: {timer() - start_timer}")
             record_stats(arrays, scores, gens, prefix="improve A2")
         #
-        """
         start_timer = timer()
         improve_phases(arrays, scores)
         scores = score(arrays)  # don't trust improve
